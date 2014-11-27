151f2e49f8b957ce31f4b8d3bf1cb781da12a4fa
cdglasz <cdglasz@ymail.com>
2014-11-27 10:11:42 -0500

    AppConstants comments

--- src/Galaga/ApplicationConstants.java
+++ src/Galaga/ApplicationConstants.java
@@ -1,24 +1,64 @@
 package Galaga;
 
+/**
+ * Interface to hold all the constants for the game
+ * 
+ * @author Christopher Glasz
+ */
 public interface ApplicationConstants {
 
-	int WINDOW_WIDTH = 800;
+	/**
+	 * Dimensions of the window
+	 */
+	int WINDOW_WIDTH = 700;
+
+	/**
+	 * Dimensions of the window
+	 */
 	int WINDOW_HEIGHT = 800;
-	
+
+	/**
+	 * Width of the world
+	 */
 	float WORLD_WIDTH = 1;
-	
-	float P2W = WORLD_WIDTH / (float)WINDOW_WIDTH;
+
+	/**
+	 * Pixel to world scaling coefficient
+	 */
+	float P2W = WORLD_WIDTH / (float) WINDOW_WIDTH;
+
+	/**
+	 * World to pixel scaling coefficient
+	 */
 	float W2P = 1.0f / P2W;
-	
+
+	/**
+	 * Height of the world
+	 */
 	float WORLD_HEIGHT = P2W * WINDOW_HEIGHT;
-	
-	float PIXEL_WIDTH = WORLD_HEIGHT/200.f;
-	
-	// Time it takes to travel from one side of the screen to the other
+
+	/**
+	 * Width of one game pixel
+	 */
+	float PIXEL_WIDTH = WORLD_HEIGHT / 255.f;
+
+	/**
+	 * Time it takes to strafe from one side of the screen to the other
+	 */
 	float CROSS_WORLD_TIME = 2;
+
+	/**
+	 * Speed at which the fighter strafes
+	 */
 	float STRAFE_SPEED = WORLD_WIDTH / CROSS_WORLD_TIME;
-	
-	// Time it takes to travel from the top of the screen to the bottom
+
+	/**
+	 * Time it takes a bullet to travel from the top of the screen to the bottom
+	 */
 	float DOWN_WORLD_TIME = 1;
+
+	/**
+	 * Speed of the bullets
+	 */
 	float BULLET_SPEED = WORLD_HEIGHT / DOWN_WORLD_TIME;
 }
