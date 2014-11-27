cbb07155642ec170f076d3f3e6a323d704058f37
cdglasz <cdglasz@ymail.com>
2014-11-27 17:04:34 -0500

    Sprites
    Created new sprites for explosions
    Updated AnimationState enum with more advanced functionality

--- src/Galaga/AnimationState.java
+++ src/Galaga/AnimationState.java
@@ -1,13 +1,34 @@
 package Galaga;
 
 public enum AnimationState {
-	up, down;
+	UP {
+		@Override
+		public AnimationState getNext() {
+			return DOWN;
+		}
+	},
+
+	DOWN {
+		@Override
+		public AnimationState getNext() {
+			return UP;
+		}
+	}, 
+
+	EXP_1, EXP_2, EXP_3, EXP_4,
+
+	EXP_5 {
+		@Override
+		public AnimationState getNext() {
+			return EXP_1;
+		}
+	};
 
 	public AnimationState getNext() {
 		return values()[(ordinal() + 1) % values().length];
 	}
 
 	public static AnimationState random() {
-		return values()[(int) (Math.random() * values().length)];
+		return values()[(int) (Math.random() * 2)];
 	}
 }
