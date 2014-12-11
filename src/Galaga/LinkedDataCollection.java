334756949118e987dae8f1d3225177910601261e
Collins-J-URI <collins501@my.uri.edu>
2014-12-11 14:52:51 -0500

    Saves Score to file. Highscore feature complete

--- src/Galaga/LinkedDataCollection.java
+++ src/Galaga/LinkedDataCollection.java
@@ -20,10 +20,25 @@ public class LinkedDataCollection {
 		if(isEmpty()){
 			head = new Node(name,score);
 			selected = tail = head;
-		}else{
+		}else if (selected == tail){ 
 			Node newNode = new Node(name,score,selected,null);
 			selected.setNext(newNode);
 			tail = selected = selected.getNext();
+		}
+			
+		
+	}
+
+	public void insert(String name, int score){	
+		
+		if(selected == head){
+			Node newNode = new Node(name,score,null,selected);
+			head = newNode;
+			selected = head;
+		}else{
+			Node newNode = new Node(name,score,selected.getPrevious(),selected);
+			selected.getPrevious().setNext(newNode);
+			selected = newNode;
 		}
 		
 	}
@@ -54,10 +69,10 @@ public class LinkedDataCollection {
 	 * Changes selected to the Node given
 	 * @param selectThis the Node to be selected
 	 */
-	public void reset(Node selectThis){
+	public void reset(Item selectThis){
 		selected = head;
 		
-		while (selected != selectThis && selected != null){
+		while (selected.getItem() != selectThis && selected != null){
 			selected = selected.getNext();
 		}
 	}
@@ -66,13 +81,14 @@ public class LinkedDataCollection {
 	 * increments selected to the next node
 	 * @return
 	 */
-	public Node next(){
+	public Item next(){
 		Node returnMe = selected;
 		if(selected != null){
 			selected = selected.getNext();
+			return returnMe.getItem();
 		}
 		
-		return returnMe;
+		return null;
 	}
 	
 	/**
