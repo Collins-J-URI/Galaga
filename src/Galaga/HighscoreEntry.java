86b3752b07bf8ab2c51f01131c912c093e48b7f4
cdglasz <cdglasz@ymail.com>
2014-12-11 22:28:19 -0500

    HighscoreEntry.java commented

--- src/Galaga/HighscoreEntry.java
+++ src/Galaga/HighscoreEntry.java
@@ -1,29 +1,76 @@
 package Galaga;
 
+/**
+ * Defines an entry in the highscore list
+ * 
+ * @author Christopher Glasz
+ */
 public class HighscoreEntry {
-	
-	private String _name;
-	private int _score;
-	
+
+	/**
+	 * Name of the player
+	 */
+	private String name;
+
+	/**
+	 * High score
+	 */
+	private int score;
+
+	/**
+	 * Default constructor does nothing
+	 */
 	public HighscoreEntry() {
-		// TODO Auto-generated constructor stub
 	}
-	public HighscoreEntry(String name, int score){
-		_name = name;
-		_score = score;
-	}
-	
 
-
-	public String getName(){return _name;}
-	
-	public int getScore(){return _score;}
-	
-	public void setName(String name){
-		_name = name;
+	/**
+	 * COnstructor initilaizes variables
+	 * 
+	 * @param name
+	 *            name of the player
+	 * @param score
+	 *            high score
+	 */
+	public HighscoreEntry(String name, int score) {
+		this.name = name;
+		this.score = score;
 	}
-	
-	public void setScore(int score){
-		_score = score;
+
+	/**
+	 * Accessor method for name
+	 * 
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Accessor method for score
+	 * 
+	 * @return score
+	 */
+	public int getScore() {
+		return score;
+	}
+
+	/**
+	 * Matator method for name
+	 * 
+	 * @param name
+	 *            name of the player
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Mutator method for the score
+	 * 
+	 * @param score
+	 *            high score
+	 */
+	public void setScore(int score) {
+		this.score = score;
 	}
 }
