52377baaa19c04816be7eadf384c953a2a5b1921
cdglasz <cdglasz@ymail.com>
2014-12-11 22:48:00 -0500

    HighscoreNode.java

--- src/Galaga/HighscoreNode.java
+++ src/Galaga/HighscoreNode.java
@@ -1,51 +1,136 @@
 package Galaga;
 
+/**
+ * Defines a node in a highscore collection
+ * 
+ * @author Christopher Glasz
+ */
 public class HighscoreNode {
-	
+
+	/**
+	 * Highscore entry
+	 */
 	private HighscoreEntry highscore;
 
-	
-	private HighscoreNode _next;
-	private HighscoreNode _previous;
-	
-	
-	public HighscoreNode(){
+	/**
+	 * Next node in the linked list
+	 */
+	private HighscoreNode next;
+
+	/**
+	 * Previous node in the linked list
+	 */
+	private HighscoreNode previous;
+
+	/**
+	 * Constructor initializes variables
+	 */
+	public HighscoreNode() {
 		highscore = new HighscoreEntry();
-		_next = null;
+		this.next = null;
 	}
-	
-	public HighscoreNode(String name, int score){
-		highscore = new HighscoreEntry(name,score);
-		_next = null;
+
+	/**
+	 * Constructor initializes variables
+	 * 
+	 * @param name
+	 *            name
+	 * @param score
+	 *            high score
+	 */
+	public HighscoreNode(String name, int score) {
+		highscore = new HighscoreEntry(name, score);
+		this.next = null;
 	}
-	
-	public HighscoreNode(String name, int score, HighscoreNode next){
-		highscore = new HighscoreEntry(name,score);
-		_next = next;
+
+	/**
+	 * Constructor initializes variables
+	 * 
+	 * @param name
+	 *            name
+	 * @param score
+	 *            high score
+	 * @param next
+	 *            next node in the linked list
+	 */
+	public HighscoreNode(String name, int score, HighscoreNode next) {
+		highscore = new HighscoreEntry(name, score);
+		this.next = next;
 	}
-	
-	public HighscoreNode(String name, int score, HighscoreNode prev, HighscoreNode next){
-		highscore = new HighscoreEntry(name,score);
-		_next = next;
-		_previous = prev;
+
+	/**
+	 * Constructor initializes variables
+	 * 
+	 * @param name
+	 *            name
+	 * @param score
+	 *            high score
+	 * @param prev
+	 *            previous node in the linked list
+	 * @param next
+	 *            next node in the linked list
+	 */
+	public HighscoreNode(String name, int score, HighscoreNode prev,
+			HighscoreNode next) {
+		highscore = new HighscoreEntry(name, score);
+		this.next = next;
+		this.previous = prev;
 	}
-	
-	public void setItem(HighscoreEntry newHighscore){
+
+	/**
+	 * Mutator method for highscore
+	 * 
+	 * @param newHighscore
+	 *            new highscore
+	 */
+	public void setItem(HighscoreEntry newHighscore) {
 		highscore = newHighscore;
 	}
 
-	
-	public void setNext(HighscoreNode next){
-		_next = next;
+	/**
+	 * Mutator method for next
+	 * 
+	 * @param next
+	 *            next node in the linked list
+	 */
+	public void setNext(HighscoreNode next) {
+		this.next = next;
 	}
-	
-	public void setPrevious(HighscoreNode prev){
-		_previous = prev;
+
+	/**
+	 * Mutator method for previous
+	 * 
+	 * @param prev
+	 *            previous node in the linked list
+	 */
+	public void setPrevious(HighscoreNode prev) {
+		this.previous = prev;
 	}
-	
-	public HighscoreEntry getItem(){return highscore;}
-	
-	public HighscoreNode getNext(){return _next;}
-	
-	public HighscoreNode getPrevious(){return _previous;}
+
+	/**
+	 * Accessor method for high score
+	 * 
+	 * @return highscore
+	 */
+	public HighscoreEntry getItem() {
+		return highscore;
+	}
+
+	/**
+	 * Accessor method for next
+	 * 
+	 * @return next
+	 */
+	public HighscoreNode getNext() {
+		return this.next;
+	}
+
+	/**
+	 * Accessor method for previous
+	 * 
+	 * @return previous
+	 */
+	public HighscoreNode getPrevious() {
+		return this.previous;
+	}
 }
